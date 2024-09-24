09dd3f01efe4b05f2a23c3cf044d18bd956f63ea
Kelmfaker <125546934+Kelmfaker@users.noreply.github.com>
2024-09-24 11:30:23 +0200

    update
    update index.html and styles.css to be more developped
    delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const alertButton = document.getElementById('alertButton');
-    alertButton.addEventListener('click', () => {
-        alert('Button clicked!');
-    });
-});
