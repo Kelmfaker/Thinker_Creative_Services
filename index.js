f1df9d3108733384aad35f3dbc14bc64a6a9eddf
Kelmfaker <kelmfaker@gmail.com>
2025-03-10 15:35:39 +0100

    update index.js connecting with index.html/ adding .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Server is running');
+const express = require("express");
+const path = require("path");
+
+
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// static files from public directory
+app.use("/frontend", express.static(path.join(__dirname, "frontend")));
+app.use(express.static('frontend')); // Ensure "frontend" folder is accessible
+
+
+// Default route to test server status
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "/frontend", "index.html"));
+});
+
+// Error handling middleware
+app.use((error, request, response, next) => {
+  console.error("Error:", error.message);
+  response.status(500).json({ error: "Internal Server Error" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
